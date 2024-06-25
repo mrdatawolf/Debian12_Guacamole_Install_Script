9623ab4e2d5ea0d480fb57b325ea632a2adb091b
Patrick Moon <patrickmoon@outlook.com>
2024-06-25 11:58:38 -0700

    fixing for v1

--- d12_guac_install.sh
+++ d12_guac_install.sh
@@ -1,20 +1,22 @@
+#!/bin/bash
 #SET THE VERSION TO THE ONE YOU WANT. 1.5.5 IS CURRENT AS OF JUN 2024
 VER=1.5.5
-apt update
-apt install -y build-essential libcairo2-dev libjpeg62-turbo-dev libpng-dev	libtool-bin uuid-dev libossp-uuid-dev libavcodec-dev libavformat-dev libavutil-dev libswscale-dev freerdp2-dev libpango1.0-dev libssh2-1-dev libvncserver-dev libtelnet-dev libwebsockets-dev libssl-dev libvorbis-dev libwebp-dev libpulse-dev sudo vim postgresql postgresql-contrib
+sed -i '/^deb cdrom:/s/^/#/' /etc/apt/sources.list
+apt-get update
+apt-get install -y build-essential libcairo2-dev libjpeg62-turbo-dev libpng-dev	libtool-bin uuid-dev libossp-uuid-dev libavcodec-dev libavformat-dev libavutil-dev libswscale-dev freerdp2-dev libpango1.0-dev libssh2-1-dev libvncserver-dev libtelnet-dev libwebsockets-dev libssl-dev libvorbis-dev libwebp-dev libpulse-dev sudo vim postgresql postgresql-contrib
 wget https://downloads.apache.org/guacamole/$VER/source/guacamole-server-$VER.tar.gz
 tar xzf guacamole-server-$VER.tar.gz
 cd guacamole-server-$VER
-configure --with-systemd-dir=/etc/systemd/system/
+./configure --with-systemd-dir=/etc/systemd/system/
 make
 make install
 ldconfig
 systemctl daemon-reload
 sed -i '/^::1/s/^/#/g' /etc/hosts
 systemctl restart guacd
-echo "deb http://deb.debian.org/debian/ bullseye main" > /etc/apt/sources.list.d/bullseye.list 
-apt update
-apt install tomcat9 tomcat9-admin tomcat9-common tomcat9-user -y
+echo "deb http://deb.debian.org/debian/ bullseye main" > /etc/apt/sources.list.d/bullseye.list
+apt-get update
+apt-get install tomcat9 tomcat9-admin tomcat9-common tomcat9-user -y
 sed -i 's/^/#/' /etc/apt/sources.list.d/bullseye.list 
 systemctl status tomcat9.service
 mkdir /etc/guacamole
@@ -40,21 +42,14 @@ cat > /etc/guacamole/user-mapping.xml << EOL
     </authorize>
 </user-mapping>
 EOL
-cd ~
 wget https://jdbc.postgresql.org/download/postgresql-42.7.3.jar -O /etc/guacamole/lib/postgresql-42.7.3.jar
 wget https://apache.org/dyn/closer.lua/guacamole/$VER/binary/guacamole-auth-jdbc-$VER.tar.gz?action=download -O guacamole-auth-jdbc-$VER.tar.gz
 tar -xvzf guacamole-auth-jdbc-$VER.tar.gz
-cd ~/guacamole-auth-jdbc-1.5.5/postgresql
-mv guacamole-auth-jdbc-postgresql-1.5.5.jar /etc/guacamole/extensions/
+cd ~/guacamole-auth-jdbc-$VER/postgresql
+mv guacamole-auth-jdbc-postgresql-$VER.jar /etc/guacamole/extensions/
 mkdir /tmp/schema
-mv schema/*.sql /tmp/
+mv ~/guacamole-auth-jdbc-$VER/postgresql/schema/*.sql /tmp/schema
 chmod 0777 -R /tmp/schema
-sudo -u postgres bash -c 'createdb guacamole_db && \
-cd /tmp && \
-cat schema/*.sql | psql -d guacamole_db -f - && \
-psql -d guacamole_db -c "CREATE USER guacamole_user WITH PASSWORD '\''some_password'\'';" && \
-psql -d guacamole_db -c "GRANT SELECT,INSERT,UPDATE,DELETE ON ALL TABLES IN SCHEMA public TO guacamole_user;" && \
-psql -d guacamole_db -c "GRANT SELECT,USAGE ON ALL SEQUENCES IN SCHEMA public TO guacamole_user;"'
 cat >  /etc/guacamole/guacamole.properties << EOL
     postgresql-hostname: localhost
     postgresql-database: guacamole_db
@@ -62,4 +57,18 @@ cat >  /etc/guacamole/guacamole.properties << EOL
     postgresql-password: some_password
     auth-provider: org.apache.guacamole.auth.postgresql.PostgreSQLAuthenticationProvider
 EOL
+sudo -u postgres createdb guacamole_db 
+cd /tmp
+sudo -u postgres bash -c "cat schema/*.sql | psql -d guacamole_db -f -"
+sudo -u postgres psql -d guacamole_db -c "CREATE USER guacamole_user WITH PASSWORD 'Apassword5';"
+sudo -u postgres psql -d guacamole_db -c "GRANT SELECT,INSERT,UPDATE,DELETE ON ALL TABLES IN SCHEMA public TO guacamole_user;"
+sudo -u postgres psql -d guacamole_db -c "GRANT SELECT,USAGE ON ALL SEQUENCES IN SCHEMA public TO guacamole_user;"
+createdb guacamole_db 
+cd /tmp
+cat schema/*.sql | psql -d guacamole_db -f -
+psql -d guacamole_db -c "CREATE USER guacamole_user WITH PASSWORD 'Apassword5';"
+psql -d guacamole_db -c "GRANT SELECT,INSERT,UPDATE,DELETE ON ALL TABLES IN SCHEMA public TO guacamole_user;"
+psql -d guacamole_db -c "GRANT SELECT,USAGE ON ALL SEQUENCES IN SCHEMA public TO guacamole_user;"
+rm -rf /var/lib/tomcat9/webapps/ROOT
+mv /var/lib/tomcat9/webapps/guacamole /var/lib/tomcat9/webapps/ROOT
 systemctl restart tomcat9 guacd
